9f29d7da6035506743be2077abbb25f3495fb761
Will Resen <willresen@gmail.com>
2019-09-04 15:05:24 -0400

    Add React Router routes to each component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Register from './Register.jsx';
+import Sidebar from './Sidebar.jsx';
+import Login from './Login.jsx';
+import Build from './Build.jsx';
+import Lend from './Lend.jsx';
+import Borrow from './Borrow.jsx';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
-  };
+  }
 
   render() {
     return (
       <Router>
-        <Sidebar />
-        <Main />
+        <Sidebar/>
+        <Main>
+          <Route path = '/register' component={Register}></Route>
+          <Route path = '/login' component={Login}></Route>
+          <Route path = '/build' component={Build}></Route>
+          <Route path = '/borrow' component={Borrow}></Route>
+          <Route path = '/lend' component={Lend}></Route>
+        </Main>
       </Router>
     );
   }
