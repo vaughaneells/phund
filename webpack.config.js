dc4d1e7d34c500866607c7e3bfbde43a8a934c3f
Will Resen <willresen@gmail.com>
2019-09-05 23:35:36 -0400

    Add runtime plugin

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,8 @@ module.exports = {
         exclude: path.resolve(__dirname, 'node_modules'),
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react']
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: ['@babel/plugin-transform-runtime']
         }
       }
     ]
