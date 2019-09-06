786c811288f9af7527f7c9137a4c6236f670c2f8
Will Resen <willresen@gmail.com>
2019-09-06 00:07:20 -0400

    Add comments

--- client/src/helpers/authenticateUser.js
+++ client/src/helpers/authenticateUser.js
@@ -1,8 +1,13 @@
 const auth = async () => {
+  //user is not authenticated by default
   let authenticated = false;
+
+  //sends an HTTP request with the httpOnly JWT cookie to the server
   await fetch('/auth')
     .then(res => res.json())
     .then(res => authenticated = res.authenticated)
+
+  //return boolean of whether user is authenticated
   return authenticated;
 };
 
