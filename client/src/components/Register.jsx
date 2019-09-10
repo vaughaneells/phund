569dd4828fbe4e1120429893ae489e1209563928
Will Resen <willresen@gmail.com>
2019-09-09 23:10:06 -0400

    Add initial form inputs

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 
-const Register = () => {
-  return <div></div>;
-}
+const Register = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div id="register">
+      <form onSubmit={onSubmit}>
+        <input type="text" value={name} placeholder="Name" onChange={e => setName(e.target.value)}></input>
+        <input type="email" value={email} placeholder="Email" onChange={e => setEmail(e.target.value)}></input>
+        <input type="password" value={password} placeholder="Password" onChange={e => setPassword(e.target.value)}></input>
+      </form>
+    </div>
+  );
+};
 
 export default Register;
