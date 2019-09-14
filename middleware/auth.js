e5bf9dc7f2f66df648f9d1ae188fd591ab06f248
Will Resen <willresen@gmail.com>
2019-09-14 13:55:06 -0400

    Retrieve token from cookie rather than header

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,12 +2,12 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 
 module.exports = (req, res, next) => {
-  //Get token from header
-  const token = req.header('x-auth-token');
+  //Get token from cookie
+  const token = req.signedCookies.access_token;
 
   //Check if not token
   if (!token) {
-    return res.status(401).json({ msg: 'No token . Authorization denied' });
+    return res.status(401).json({ msg: 'No token. Authorization denied' });
   }
 
   //Verify token
