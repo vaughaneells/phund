4678ebd74342c649870bce26e9928cf3cc48ad93
Will Resen <willresen@gmail.com>
2019-09-15 01:00:28 -0400

    Authenticate based on cookie, not JWT

--- client/src/components/PrivateComponent.jsx
+++ client/src/components/PrivateComponent.jsx
@@ -3,7 +3,7 @@ import { Redirect, Route } from 'react-router-dom';
 import auth from '../helpers/authenticateUser.js';
 
 const PrivateComponent = (component) => {
-  return React.createElement(component);
+  return document.cookie.toString().includes('logged_in=true') ? React.createElement(component) : <Redirect to='/login'/>
 };
 
 export default PrivateComponent;
