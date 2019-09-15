d9fa91d01cc142834ea211eb9f60a03ab87a5e0a
Will Resen <willresen@gmail.com>
2019-09-15 15:00:09 -0400

    Remove auth function and replace with document.cookie

--- client/src/components/PrivateComponent.jsx
+++ client/src/components/PrivateComponent.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
-import auth from '../helpers/authenticateUser.js';
 
 const PrivateComponent = (component) => {
-  return document.cookie.toString().includes('logged_in=true') ? React.createElement(component) : <Redirect to='/login'/>
+  return document.cookie.includes('logged_in=true') ? React.createElement(component) : <Redirect to='/login'/>
 };
 
 export default PrivateComponent;
