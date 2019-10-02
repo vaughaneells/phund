5c4733470d2d25dc319b0d13533292f59b6b41f9
vaughaneells <54754509+vaughaneells@users.noreply.github.com>
2019-10-02 01:35:42 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const connectDB = require('./config/db');
 const cookieParser = require('cookie-parser');
 const auth = require('./middleware/auth');
 const path = require('path');
+const plaid = require('./routes/api/plaid');
+const { receivePublicToken, getTransactions } = require('./routes/api/plaid');
 
 const app = express();
 
@@ -20,9 +22,18 @@ app.use(express.static('./client/dist'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/login', require('./routes/api/login'));
 app.use('/api/borrower', require('./routes/api/borrower'));
+//app.use('/api/plaid', plaid);
+
+// Get the public token and exchange it for an access token
+app.post('/public_token', receivePublicToken);
+
+// Get Transactions
+app.get('/transactions', getTransactions);
 
 //Routes for React Router
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname, './client/dist/index.html')));
+app.get('/*', (req, res) =>
+  res.sendFile(path.join(__dirname, './client/dist/index.html'))
+);
 
 const PORT = process.env.PORT || 5000;
 
