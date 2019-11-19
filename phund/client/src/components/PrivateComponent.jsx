208791bdb68b0c3b750c82e59cddb8d7b56f00b9
Vaughan Eells <vaughaneells@gmail.com>
2019-11-18 22:01:03 -0800

    Updated

--- phund/client/src/components/PrivateComponent.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
-
-const PrivateComponent = (component) => {
-  return document.cookie.includes('logged_in=true') ? React.createElement(component) : <Redirect to='/login'/>
-};
-
-export default PrivateComponent;
