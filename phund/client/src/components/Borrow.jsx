d98391d96f78fd2ea270b37ad0bc7e874eae994a
Vaughan Eells <vaughaneells@gmail.com>
2019-11-18 21:10:10 -0800

    Update

---
+++ phund/client/src/components/Borrow.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Borrow = () => {
+  return <div></div>
+}
+
+export default Borrow;
---
+++ phund/models/Borrower.js
@@ -0,0 +1,94 @@
+const mongoose = require('mongoose');
+
+const BorrowerSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  name: {
+    type: String
+  },
+  loan_amount: {
+    type: Number
+  },
+  issue_date: {
+    type: Date
+  },
+  original_total_amount: {
+    type: Number
+  },
+  current_total_amount: {
+    type: Number
+  },
+  months: {
+    type: Number
+  },
+  apr: {
+    type: Number
+  },
+  issue_date: {
+    type: Date
+  },
+  amount_payed: {
+    type: Number,
+    default: 0.0
+  },
+  installments_payed: {
+    type: Number,
+    default: 0
+  },
+  installment_amount: {
+    type: [Number]
+  },
+  installment_dates: {
+    type: [Date]
+  },
+  upcoming_amount: {
+    type: Number
+  },
+  upcoming_date: {
+    type: Date
+  },
+  balance: {
+    type: Number
+  },
+  missed_payments: {
+    type: Number,
+    default: 0
+  },
+  zipcode: {
+    type: Number
+  },
+  kyc: [
+    {
+      legal_name: {
+        type: String,
+        required: true
+      },
+      address: {
+        type: String,
+        required: true
+      },
+      ssn: {
+        type: Number,
+        required: true
+      },
+      dob: {
+        type: Date,
+        required: true
+      }
+    }
+  ],
+  approved: {
+    type: Boolean
+  },
+  borrower_status: {
+    type: Boolean,
+    default: false
+  },
+  payment_successful: {
+    type: Boolean
+  }
+});
+
+module.exports = Borrower = mongoose.model('borrower', BorrowerSchema);
