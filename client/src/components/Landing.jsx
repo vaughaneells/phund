e18bb1bae5b977fe5c4ee1b99e8c72fcbf1dad2b
Jesus DeLintt <jesus.delintt@gmail.com>
2020-10-13 16:30:56 -0700

    front end changes

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
 import "antd/dist/antd.css";
-import { Layout, Menu  } from "antd";
-import { Router, Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { history } from '../helpers';
-import { userActions } from '../actions';
+import { Layout, Menu } from "antd";
+import { Router, Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { history } from "../helpers";
+import { userActions } from "../actions";
+import DivImage from './Borrow/utils/SVG/DIVSix'
+import MenuFooter from './Borrow/utils/Landing/MenuFooter'
 
-const { Header, Footer, Content} = Layout;
+const { Header, Footer, Content } = Layout;
 
 class Landing extends React.Component {
   constructor(props) {
@@ -17,18 +19,57 @@ class Landing extends React.Component {
   render() {
     return (
       <div>
-          <Router history={history}>
+        <Router history={history}>
           <Layout>
-              <Header>
-                  <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-                      <Menu.Item key="1"><Link to='/login'>login</Link></Menu.Item>
-                      <Menu.Item key="2"><Link to='/register'>register</Link></Menu.Item>
-                  </Menu>
-              </Header>
-              <Content>DIV 1</Content>
-              <Footer>Footer</Footer>
-            </Layout>
-          </Router>
+            <Header>
+              <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
+                <Menu.Item key="1">
+                  <Link to="/login">login</Link>
+                </Menu.Item>
+                <Menu.Item key="2">
+                  <Link to="/register">register</Link>
+                </Menu.Item>
+              </Menu>
+            </Header>
+            <Content>
+              <div
+                style={{
+                  background: "purple",
+                  height: "100px",
+                }}
+              >
+                text
+              </div>
+            </Content>
+            <Content>
+              <div style={{ background: "green", height: "100px" }}>text</div>
+            </Content>
+            <Content>
+              <div style={{ background: "success", height: "100px" }}>text</div>
+            </Content>
+            <Content>
+              <div style={{ background: "red", height: "100px" }}>text</div>
+            </Content>
+            <Content>
+              <div style={{ background: "red", height: "100px" }}>text</div>
+            </Content>
+            <Footer>
+              <div
+                style={{
+                  width: "1680px",
+                  height: "1126px",
+                  left: "0px",
+                  top: "4274px",
+                  background: "#E7F4FD",
+                }}
+              >
+                HELLLOOOO HIIIIIIIIIII
+                <DivImage></DivImage>
+                <MenuFooter>HEY THERE</MenuFooter>
+              </div>
+            </Footer>
+          </Layout>
+        </Router>
       </div>
     );
   }
@@ -40,7 +81,7 @@ function mapState(state) {
 }
 
 const actionCreators = {
-  logout: userActions.logout
+  logout: userActions.logout,
 };
 
 const connectedLanding = connect(mapState, actionCreators)(Landing);
