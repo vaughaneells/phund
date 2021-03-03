db352a530a2533e7441586572a733c07cf8957d1
Jesus DeLintt <jesus.delintt@gmail.com>
2021-03-03 15:15:45 -0800

    Front end flow created for userInput created
    Co-authored-by: Jimmy-Lin-91 <Jimmy-Lin-91@users.noreply.github.com>

--- client/src/components/userInput/createProfile.js
+++ client/src/components/userInput/createProfile.js
@@ -1,43 +1,72 @@
 import {Input} from 'antd'
 import React, { useState, useEffect } from "react";
 import socketIOClient from "socket.io-client";
+import SSN from './ssn';
+import Address from './address.js';
+import Driver from './driversLicense.js';
 const ENDPOINT = "http://localhost:3000";
 
-function createProfile() {
-  const [ ssn, setSsn ] = useState("");
-  const [ photoId, setPhotoId ] = useState("");
-  const [ address, setAddress ] = useState("");
-  const [response, setResponse] = useState("");
-  
-  var handleSSN = (e) => {
-    setSsn({
+class CreateProfile extends React.Component {
+  constructor() {
+    super(),
+    this.state = {
+      step: 'create',
+      ssn: '',
+      photoid: '',
+      address: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.onCreate = this.onCreate.bind(this);
+  }
+
+   
+  handleChange(e) {
+    this.setState({
         [e.target.name] : e.target.value
     })
-    
   }
-  var submitSSN =(e) => {
-      e.preventDefault();
-      console.log('SUBMITTED CORRECTLY');
-    const socket = socketIOClient(ENDPOINT);
-      socket.emit('data', ssn)
+  onCreate(e) {
+    this.setState({
+      step: 'ssn'
+    }, () => console.log(this.state))
   }
-  useEffect(() => {
+  componentDidMount() {
     const socket = socketIOClient(ENDPOINT);
     socket.on("show", (data) => {
         console.log(data);
     });
-  }, []);
+  }
+  render(){
   
-  return (
-    <Input
-    onChange={handleSSN}
-    onPressEnter={submitSSN}
-    name='ssn'
-    type='text'
-    >
-      
-    </Input >
-  );
+    if (this.state.step === 'create') {
+      return (
+        <Input
+        type="text" 
+        placeholder="Create"
+        onPressEnter={() => this.onCreate()}
+        /> 
+      )
+    } else if (this.state.step === 'ssn') {
+      return (
+        <div>
+          <SSN handleChange={this.handleChange}> </SSN>
+        </div>
+      )
+    } else if (this.state.step === 'address') {
+      return (
+        <div>
+          <Address handleChange={this.handleChange}></Address>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <Driver handleChange={this.handleChange}> </Driver>
+        </div>
+      )
+    }
+  }
 }
 
-export default createProfile;
+
+export default CreateProfile;
---
+++ client/src/components/userInput/ssn.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Form, Input } from 'antd'
+
+
+function SSN() {
+   
+    return(
+        <div>
+            <Form>
+                <Form.Item>
+                    <Input placeholder='SSN'></Input>
+                </Form.Item>
+            </Form>
+        
+        </div>
+    )
+}
+    export default SSN
