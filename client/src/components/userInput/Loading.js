9d0cf6383b8caa47d995e3f52c82ff679df958c9
Jesus DeLintt <jesus.delintt@gmail.com>
2021-03-06 14:37:10 -0800

    created UI style for profilecreation
    Co-authored-by: Jimmy-Lin-91 <Jimmy-Lin-91@users.noreply.github.com>

---
+++ client/src/components/userInput/Loading.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Icon from '@ant-design/icons';
+import customIcon from '../Borrow/utils/Landing/Assets/SVG/AirplaneIcon';
+
+
+const Loading = () => {
+return (
+  <Icon 
+  spin
+  component={customIcon}/>
+)
+}
+
+
+export default Loading;
--- client/src/components/userInput/createProfile.js
+++ client/src/components/userInput/createProfile.js
@@ -1,10 +1,15 @@
-import {Input} from 'antd'
+import { Button, Layout, Row, Col } from 'antd'
+import Icon from '@ant-design/icons';
 import React, { useState, useEffect, prevState } from "react";
 import socketIOClient from "socket.io-client";
 import SSN from './ssn';
 import Address from './address.js';
 import Driver from './driversLicense.js';
 const ENDPOINT = "http://localhost:3000";
+const { Content } = Layout;
+import customIcon from '../Borrow/utils/Landing/Assets/SVG/AirplaneIcon'
+import { FormProvider } from 'antd/lib/form/context';
+import Loading from './Loading.js';
 
 class CreateProfile extends React.Component {
   constructor() {
@@ -13,13 +18,14 @@ class CreateProfile extends React.Component {
       step: 1,
       ssn: '',
       photoid: '',
-      address: ''
+      address: '',
+      loading: false
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleNext = this.handleNext.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-
+  
 
   handleChange(e) {
     this.setState({
@@ -28,8 +34,18 @@ class CreateProfile extends React.Component {
   }
   handleNext() {
     this.setState({
-      step: this.state.step+1,
-    }, () => {console.log(this.state.step)})
+       loading: true,
+    });
+    setTimeout(() => {
+      this.setState({
+        loading: false
+      })
+    }, 1500);
+    setTimeout(() => {
+      this.setState({
+        step: this.state.step + 1,
+      })
+    }, 2500)
   }
   componentDidMount() {
     const socket = socketIOClient(ENDPOINT);
@@ -45,49 +61,106 @@ class CreateProfile extends React.Component {
       photoid: this.state.photoid
     }
   }
-  render(){
 
-    if (this.state.step === 1) {
+  
+  render(){
+  const loadingToggle = () => {
+    if (this.state.loading) {
+      return (
+        <Loading />
+      )
+    }
+  }
+  if (this.state.step === 1) {
       return (
         <div>
-          <Input
-          type="text"
-          placeholder="Create"
-          onPressEnter={this.handleNext}
-          />
+          <Layout>
+            <Content style={styles.ContentStyle}>
+              <Row>
+                <Col>
+                  <h1
+                    style={styles.h1}
+                  >
+                    Click the button below to create your profile
+                  </h1>
+                  <Button onClick={this.handleNext} style={styles.button}>Click here</Button>
+                </Col>
+              </Row>
+              {loadingToggle()}
+            </Content>
+          </Layout>
         </div>
-
       )
     } else if (this.state.step === 2) {
       return (
         <div>
-          <SSN
-          handleChange={this.handleChange}
-          handleNext={this.handleNext}
-          > </SSN>
+          <Layout>
+            <Content style={styles.ContentStyle}>
+              <SSN
+              handleChange={this.handleChange}
+              handleNext={this.handleNext}
+              > </SSN>
+              {loadingToggle()}
+            </Content>
+          </Layout>
         </div>
       )
     } else if (this.state.step === 3) {
       return (
         <div>
-          <Address
-          handleChange={this.handleChange}
-          handleNext={this.handleNext}
-          ></Address>
+          <Layout>
+            <Content style={styles.ContentStyle}>
+              <Address
+              handleChange={this.handleChange}
+              handleNext={this.handleNext}
+              ></Address>
+              {loadingToggle()}             
+            </Content>
+          </Layout>
         </div>
       )
     } else {
       return (
         <div>
-          <Driver
-          handleChange={this.handleChange}
-          handleSubmit={this.handleSubmit}
-          > </Driver>
+          <Layout>
+            <Content style={styles.ContentStyle}>
+              <Driver
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              > </Driver>
+             {/* put onSubmit Button here */}
+            </Content>
+          </Layout>
         </div>
       )
     }
   }
 }
-
+const styles = {
+  ContentStyle: {
+    background: '#2A2958', 
+    height: '900px', 
+    padding: '50px'
+  },
+  h1: {
+    fontWeight:'bold', 
+    fontSize: '30px', 
+    color: '#ffffff'
+  },
+  button: {
+    width: "250px",
+    height: "40px",
+    marginTop:"10",
+    paddingTop: "15",
+    paddingBottom: "15",
+    marginLeft: "30",
+    marginRight: "30",
+    backgroundColor:'#FF5776',
+    borderRadius: "25px",
+    borderWidth: "1",
+    color: '#ffffff',
+    justifyContent: 'center'
+  }
+}
 
 export default CreateProfile;
